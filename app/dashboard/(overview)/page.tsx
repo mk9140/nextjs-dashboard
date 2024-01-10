513d0dd8a16622a2baa586854379dec46dfafc9e
m-ki <jpmk9140@gmail.com>
2024-01-10 18:00:21 +0900

    LatestInvoices 컴포넌트 스트리밍

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -3,17 +3,19 @@ import RevenueChart from '@/app/ui/dashboard/revenue-chart';
 import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
 import { lusitana } from '@/app/ui/fonts';
 // import {fetchRevenue, fetchLatestInvoices, fetchCardData} from '@/app/lib/data'; // 데이터베이스에서 데이터를 가져오는 쿼리(select문)를 실행하는 함수
-import { fetchLatestInvoices, fetchCardData} from '@/app/lib/data'; // fetchRevenue (느린 데이터요청을 상정한 함수)는 Suspense를 사용하기위해 제외했다.
+// import { fetchLatestInvoices, fetchCardData} from '@/app/lib/data'; // fetchRevenue (느린 데이터요청을 상정한 함수)는 Suspense를 사용하기위해 제외했다.
+import { fetchCardData} from '@/app/lib/data'; // fetchLatestInvoices는 Suspense를 사용하기위해 제외했다.
+
 
 import { Suspense } from 'react'; // Suspense 컴포넌트를 임포트
-import { RevenueChartSkeleton } from '@/app/ui/skeletons'; // RevenueChart를 표시할 부분의 스켈레톤 UI 컴포넌트를 임포트
+import { RevenueChartSkeleton, LatestInvoicesSkeleton } from '@/app/ui/skeletons'; // 로딩중 표시할 UI인, 스켈레톤 UI 컴포넌트를 임포트
 
 // 비동기로 데이터를 습득하기 위해 async/await 사용
 export default async function Page() {
   // 데이터 가져오기의 경우 각 요청은 이전 요청이 데이터를 반환한 후에만 시작할 수 있습니다.(request waterfalls)
 
-  // const revenue = await fetchRevenue(); // fetchRevenue (느린 데이터요청을 상정한 함수)는 Suspense를 사용하기위해 제외했다.
-  const latestInvoices = await fetchLatestInvoices(); // fetchRevenue() 가 종료된 다음 실행된다.
+  // const revenue = await fetchRevenue(); // fetchRevenue (느린 데이터요청을 상정한 함수)는 Suspense를 사용하기 위해(컴포넌트 내부에서 fetch) 제외했다.
+  // const latestInvoices = await fetchLatestInvoices(); // Suspense를 사용하기 위해(컴포넌트 내부에서 fetch) 제외했다.
 
   // 일반적인 변수 할당 방법
   // const cardData = await fetchCardData();
@@ -57,7 +59,11 @@ export default async function Page() {
           {/* 이제, RevenueChart 에서 데이터를 다 받기 전에도 RevenueChart 이외의 부분은 렌더링된다! */}
           <RevenueChart />
         </Suspense>
-        <LatestInvoices latestInvoices={latestInvoices} />
+
+        {/* 연습. LatestInvoices 도 스트리밍해보자.*/}
+        <Suspense fallback={<LatestInvoicesSkeleton />}>
+          <LatestInvoices />
+        </Suspense>
       </div>
     </main>
   );
--- app/ui/dashboard/latest-invoices.tsx
+++ app/ui/dashboard/latest-invoices.tsx
@@ -3,11 +3,17 @@ import clsx from 'clsx';
 import Image from 'next/image';
 import { lusitana } from '@/app/ui/fonts';
 import { LatestInvoice } from '@/app/lib/definitions';
-export default async function LatestInvoices({
-  latestInvoices,
-}: {
-  latestInvoices: LatestInvoice[];
-}) {
+import { fetchLatestInvoices } from '@/app/lib/data';
+
+// props로 latestInvoices를 받아오는 방식에서, 컴포넌트 내부에서 latestInvoices를 가져오는 방식으로 변경
+// export default async function LatestInvoices({
+//   latestInvoices,
+// }: {
+//   latestInvoices: LatestInvoice[];
+// }) {
+export default async function LatestInvoices() {
+  const latestInvoices = await fetchLatestInvoices();
+
   return (
     <div className="flex w-full flex-col md:col-span-4">
       <h2 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
