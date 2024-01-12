9262c5f024879702979f49075dd7685489f9c609
m-ki <jpmk9140@gmail.com>
2024-01-12 23:38:28 +0900

    validation

--- app/ui/invoices/create-form.tsx
+++ app/ui/invoices/create-form.tsx
@@ -10,10 +10,24 @@ import {
 } from '@heroicons/react/24/outline';
 import { Button } from '@/app/ui/button';
 import { createInvoice } from '@/app/lib/actions';
+import { useFormState } from 'react-dom';
+
 
 export default function Form({ customers }: { customers: CustomerField[] }) {
+  // The initialState can be anything you define, in this case, create an object with two empty keys: message and errors.
+  const initialState = { message: null, errors: {} };
+
+  // useFormState에는 두 개의 인수가 필요하다.
+  // - action : 폼을 제출할 때 실행할 함수
+  // - initialState : 폼의 초기 상태
+  const [state, dispatch] = useFormState(createInvoice, initialState);
+  // useFormState은 두 개의 값이 있는 배열을 반환한다.
+  // - state : 폼의 현재 상태
+  // - dispatch : 폼 컴포넌트의 action 속성 또는 폼 내 버튼 컴포넌트의 formAction 속성으로 전달할 수 있는 새로운 액션 객체입니다. 이 액션을 사용하여 폼의 상태를 변경하고 업데이트할 수 있습니다.
+
+
   return (
-    <form action={createInvoice}>
+    <form action={dispatch}>
       <div className="rounded-md bg-gray-50 p-4 md:p-6">
         {/* Customer Name */}
         <div className="mb-4">
@@ -26,6 +40,7 @@ export default function Form({ customers }: { customers: CustomerField[] }) {
               name="customerId"
               className="peer block w-full cursor-pointer rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500"
               defaultValue=""
+              aria-describedby="customer-error"
             >
               <option value="" disabled>
                 Select a customer
@@ -38,6 +53,14 @@ export default function Form({ customers }: { customers: CustomerField[] }) {
             </select>
             <UserCircleIcon
               className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500"/>
+          </div>
+          <div id="customer-error" aria-live="polite" aria-atomic="true">
+            {state.errors?.customerId &&
+              state.errors.customerId.map((error: string) => (
+                <p className="mt-2 text-sm text-red-500" key={error}>
+                  {error}
+                </p>
+              ))}
           </div>
         </div>
 
@@ -55,6 +78,7 @@ export default function Form({ customers }: { customers: CustomerField[] }) {
                 step="0.01"
                 placeholder="Enter USD amount"
                 className="peer block w-full rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500"
+                required // 클라이언트 사이드의 유효성 검사(필수입력)
               />
               <CurrencyDollarIcon
                 className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900"/>
