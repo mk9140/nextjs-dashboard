baf7d60440b415add31c8e7209f4085f54353fad
m-ki <jpmk9140@gmail.com>
2024-02-08 11:56:10 +0900

    메타데이터 추가의 Config-based 방식

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,17 @@
 import '@/app/ui/global.css'; // 글로벌 스타일. 사이트 전체 스타일 등과 같은 CSS 규칙을 정의. 일반적으로 최상위의 구성 요소(즉, 루트 레이아웃)에 추가하는 것이 좋다.
 import { inter } from '@/app/ui/fonts'; // 폰트를 임포트.
+import { Metadata } from 'next'; // 메타데이터 작성
+
+// 메타데이터의 Config-based 방식
+export const metadata: Metadata = {
+  title: {
+    // template: 각 페이지에 대한 템플. %s 자리가 각 페이지에서 정의한 title 의 값으로 대체된다.
+    template: '%s | Acme Dashboard',
+    default: 'Acme Dashboard',
+  },
+  description: 'The official Next.js Learn Dashboard built with App Router.',
+  metadataBase: new URL('https://next-learn-dashboard.vercel.sh'),
+};
 
 // 루트 레이아웃. 필수. 모든 페이지에서 공유되는 레이아웃.
 // 주로 메타데이터와 <html>태그, <body>태그를 포함한다.
